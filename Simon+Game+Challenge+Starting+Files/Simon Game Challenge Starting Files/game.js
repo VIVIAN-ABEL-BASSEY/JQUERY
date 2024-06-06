253771dec6deda6f5816918d99efc3f7cbb51553
VIVIAN BASSEY <vivianbasseya@gmail.com>
2024-06-06 02:51:18 +0100

    yes

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,19 @@
-alert("i'm active")
+// alert("i'm active")
+// $("h1").css("color","red")
+let buttonColours =["red", "blue", "green", "yellow"]
+let gamePattern =[]
+function nextSequence(){
+     alert("fun")
+    let randomNumber = Math.floor(Math.random()*4)
+    let randomChosenColour = buttonColours[randomNumber]
+      console.log(randomChosenColour)
+    gamePattern.push(randomChosenColour)
+    // console.log(gamePattern)
+    $("button#red").css('background-color',"black")
+    // $("div#randomChosenColour").on("click",function(){
+    //     $("div#randomChosenColour").css('background-color',"black")
+    // }
+    // )
+}
+
+nextSequence()
